db429bacfcf7ab49aea09ddc5a03925c1c180fc0
farisabu3ram <aburamf@gmail.com>
2020-01-24 19:51:23 +0200

    Rewrite the form

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -9,22 +9,67 @@ import { Router } from '@angular/router';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
-
+  showMessage = 'none';
+  showMessagePass = 'none';
+  message: string;
+  messagePass: string;
   constructor(private rout: Router) { }
   form = new FormGroup({
     email: new FormControl('', [Validators.maxLength(180), Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@+(pseu.edu)")]),
-    password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{5,11}')])
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{5,11}')])
   })
 
   ngOnInit() {
   }
   logIn() {
+    event.preventDefault();
     if (this.email.valid && this.password.valid)
       this.rout.navigate(['/home']);
+    else {
+      if (this.email.errors && this.email.errors.required) {
+        this.message = `Email is required.`
+        this.showMessage = 'flex'
+      }
+      else if (this.email.errors && this.email.errors.maxlength) {
+        this.message = `Email can't be over 180digit.`;
+        this.showMessage = 'flex'
 
+      }
+      else if (this.email.invalid) {
+        this.message = `The Email end of @pseu.edu`;
+        this.showMessage = 'flex';
+      }
+      else
+        this.showMessage = 'none';
 
+      if (this.password.errors && this.password.errors.required) {
+        this.messagePass = `Password is required.`;
+        this.showMessagePass = 'flex';
+      }
+      else if (this.password.errors && this.password.errors.minlength) {
+        this.messagePass = `Password  at least 6 digit.`;
+        this.showMessagePass = 'flex'
 
+      }
+      else if (this.password.errors && this.password.errors.maxlength) {
+        this.messagePass = `Password can't be over 12 digit.`;
+        this.showMessagePass = 'flex'
 
+      }
+      else if (this.password.invalid) {
+        this.messagePass = `Use a mix of letters, numbers & symbols`;
+        this.showMessagePass = 'flex';
+      }
+      else
+        this.showMessagePass = 'none';
+
+    }
+  }
+  disapear() {
+    this.showMessage = 'none';
+  }
+  disapearPass() {
+    this.showMessagePass = 'none';
   }
   get email() {
     return this.form.get('email');
@@ -32,4 +77,5 @@ export class LogInComponent implements OnInit {
   get password() {
     return this.form.get('password');
   }
+
 }
